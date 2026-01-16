b1f327fd28e8e81c51c7c18b393069ab9f4c88ad
makszemskov-del <makszemskov@gmail.com>
2026-01-16 12:49:14 +0300

    Add files via upload

---
+++ fence-calc-pwa/sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'fence-calc-v1';
+const FILES = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    if (res.ok) { const c = res.clone(); caches.open(CACHE).then(cache => cache.put(e.request, c)); }
+    return res;
+  }).catch(() => caches.match('./index.html'))));
+});
